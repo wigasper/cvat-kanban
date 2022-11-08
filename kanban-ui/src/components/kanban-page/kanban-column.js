a4ab11c0530cd5d527796da911e68628c8e070e2
William Gasper <wkg@williamgasper.com>
2022-11-08 16:57:50 -0600

    getting basics working, render page component and columns

--- kanban-ui/src/components/kanban-page/kanban-column.js
+++ kanban-ui/src/components/kanban-page/kanban-column.js
@@ -2,16 +2,16 @@ import React, { useState, useEffect } from "react";
 
 import { getColumn } from "../../services/column";
 
-import 'antd/dist/antd.css';
+//import 'antd/dist/antd.css';
 
 import { Card } from "antd";
 
-function KanbanColumnComponent() {
+function KanbanColumnComponent({ columnID }) {
 	const [column, setColumn] = useState({ name: "" });
   
   useEffect(() => {
     let mounted = true;
-    getColumn("1")
+    getColumn(columnID)
       .then((res) => {
         if (mounted) {
           setColumn(res)
@@ -20,11 +20,11 @@ function KanbanColumnComponent() {
       .catch((err) => console.log(err));
 
     return () => (mounted = false);
-  }, []);
+  }, [columnID]);
 
   const renderColumn = () => {
     return (
-      <Card title={column.name} style={{ width: 300 }}>
+      <Card title={column.name} style={{ width: 600 }}>
         <p>tester</p>
       </Card>
     );
---
+++ kanban-ui/src/components/kanban-page/kanban-page.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+
+import KanbanColumnComponent from "./kanban-column";
+
+import { getColumns } from "../../services/column";
+
+import { Col, Row } from 'antd';
+
+import 'antd/dist/antd.css';
+
+function KanbanPageComponent() {
+  const [columns, setColumns] = useState([ ]);
+  
+  useEffect(() => {
+    let mounted = true;
+    getColumns()
+      .then((res) => {
+        if (mounted) {
+          setColumns(res)
+        }
+      })
+      .catch((err) => console.log(err));
+
+    return () => (mounted = false);
+  }, []);
+  
+  const renderColumns = () => {
+    return columns.map((column) => (
+      <Col key={column.id} xs={{ span: 5, offset: 1 }} lg={{ span: 6, offset: 2 }}>
+        <KanbanColumnComponent columnID={column.id} /> 
+      </Col>
+    ));
+  };
+  
+  const renderPage = () => {
+    return (
+      <Row>
+        {columns.map((column) => (
+          <Col key={column.id} xs={{ span: 5, offset: 1 }} lg={{ span: 6, offset: 2 }}>
+            <KanbanColumnComponent columnID={column.id} /> 
+          </Col> 
+        ))}
+      </Row>
+    );
+  };
+
+  return (
+    <div>
+      {renderPage()}
+    </div>
+  );
+
+}
+
+export default KanbanPageComponent;
