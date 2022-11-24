4e42450d9c1f86b7e77301ce28533d516d23f922
William Gasper <wkg@williamgasper.com>
2022-11-24 08:51:43 -0600

    basic drag and drop functionality

--- kanban-ui/src/components/kanban-page/kanban-column.js
+++ kanban-ui/src/components/kanban-page/kanban-column.js
@@ -4,7 +4,7 @@ import { getColumn } from "../../services/column";
 
 import { Droppable } from "react-beautiful-dnd";
 
-import 'antd/dist/antd.css';
+import "antd/dist/antd.css";
 
 import { Card } from "antd";
 
@@ -12,16 +12,20 @@ import KanbanCardComponent from "./kanban-card";
 
 import AddCardModalComponent from "./add-card-modal";
 
-import { Row, Button, Modal, Form, Input } from 'antd';
+import { Row, Button, Modal, Form, Input } from "antd";
 
-function KanbanColumnComponent({ columnID }) {
-	const [column, setColumn] = useState({ name: "a" , cards: []});
-  const [refreshCount, setRefreshCount] = useState(0);
-  
-  const getCol = mounted => {
-     getColumn(columnID)
+// TODO fix modal situation
+// TODO there can be errors for screwed up initial indices, probably
+// default for all card positions should be 0 or need logic to figure
+// out by column
+function KanbanColumnComponent({ column }) {
+  /*  const [column, setColumn] = useState(thisColumn);
+  const [loading, setLoading] = useState(false);
+
+  const getCol = () => {
+     getColumn(column.id)
       .then((res) => {
-        if (mounted) {
+        if (loading) {
           setColumn(res)
         }
       })
@@ -29,43 +33,39 @@ function KanbanColumnComponent({ columnID }) {
   };
 
   useEffect(() => {
-    let mounted = true;
+    //let mounted = true;
     
-    getCol(mounted);
+    getCol();
     
-    return () => (mounted = false);
-  }, [columnID]);
- 
+    return () => (setLoading(false));
+  }, [column]);
+*/
   const renderColumn = () => {
     return (
       <>
         <Droppable droppableId={column.name}>
-          {provided => (
-            <Card 
-              title={column.name} 
-              style={{ width: 300 }} 
+          {(provided) => (
+            <Card
+              title={column.name}
+              style={{ width: 300 }}
               ref={provided.innerRef}
               {...provided.droppableProps}
             >
               {column.cards.map((card, index) => (
                 <Row key={card.id}>
-                  <KanbanCardComponent card={card} index={index}/>
+                  <KanbanCardComponent card={card} index={index} />
                 </Row>
               ))}
               {provided.placeholder}
             </Card>
           )}
         </Droppable>
-        <AddCardModalComponent columnID={columnID} onSubmit={getCol} />
+        <AddCardModalComponent columnID={column.id} />
       </>
     );
   };
-  
-  return (
-  <div>
-    {renderColumn()}
-  </div>
-  );
+  //
+  return <div>{renderColumn()}</div>;
 }
 
 export default KanbanColumnComponent;
--- kanban-ui/src/components/kanban-page/kanban-page.js
+++ kanban-ui/src/components/kanban-page/kanban-page.js
@@ -8,47 +8,30 @@ import { getColumns } from "../../services/column";
 import { patchCard } from "../../services/card";
 import { getBoard } from "../../services/board";
 
-import { Col, Row } from 'antd';
+import { Col, Row } from "antd";
 
-import 'antd/dist/antd.css';
+import "antd/dist/antd.css";
 
 function KanbanPageComponent() {
-  const [columns, setColumns] = useState([ ]);
+  const [columns, setColumns] = useState([{ name: "a", cards: [], id: 0 }]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    //setTimeout(() => {
-    //  setLoading(true);
-    //}, 3000);
-    //setLoading(true);
-    // let loading = true;
     getBoard(1)
       .then((res) => {
         if (loading) {
-          console.log(res.columns);
           setColumns(res.columns);
-          console.log(columns);
         }
       })
       .catch((err) => console.log(err));
 
-    return () => (setLoading(false));
+    return () => setLoading(false);
   }, [columns]);
-  
- /* const renderColumns = () => {
-    return columns.map((column) => (
-      <Col key={column.id} xs={{ span: 5, offset: 1 }} lg={{ span: 6, offset: 2 }}>
-        <KanbanColumnComponent columnID={column.id} /> 
-      </Col>
-    ));
-  };*/
-  
-  const onDragEnd = res => {
-    console.log(res);
+
+  const onDragEnd = (res) => {
     const { destination, source, draggableId } = res;
 
     if (!destination) {
-      console.log("hit no dest arm");
       return;
     }
 
@@ -56,69 +39,53 @@ function KanbanPageComponent() {
       destination.droppableId === source.droppableId &&
       destination.index === source.index
     ) {
-      console.log(destination.index);
-      console.log(source.index);
-      console.log("hit same spot arm");
       return;
     }
-    
-    const thisColumn = columns.filter(function(col) {
-      return col.name === source.droppableId
+
+    const thisColumn = columns.filter(function (col) {
+      return col.name === source.droppableId;
     })[0];
-    console.log(thisColumn);
+
     const thisColumnIndex = columns.indexOf(thisColumn);
-    
-    const thisCard = thisColumn.cards[source.index];
 
-    const newCards = Array.from(thisColumn.cards);
-    newCards.splice(source.index, 1);
-    newCards.splice(destination.index, 0, thisCard);
-    
-    thisColumn.cards = newCards;
-    
-    thisColumn.cards.forEach((card, index) => {
+    /// revised
+    const newColumns = [...columns];
+
+    const thisCard = { ...newColumns[thisColumnIndex].cards[source.index] };
+    newColumns[thisColumnIndex].cards.splice(source.index, 1);
+    newColumns[thisColumnIndex].cards.splice(destination.index, 0, thisCard);
+
+    newColumns[thisColumnIndex].cards.forEach((card, index) => {
       if (card.position !== index) {
-        //card.position = index;
-        patchCard(card.id, {"position": index});
+        patchCard(card.id, { position: index });
+        const newCard = { ...newColumns[thisColumnIndex].cards[index] };
+        newCard.position = index;
+        newColumns[thisColumnIndex].cards[index] = newCard;
       }
-    }
-    );
+    });
 
-    const columnsCopy = Array.from(columns);
-    columnsCopy[thisColumnIndex] = thisColumn;
-    console.log("THIS COLUMN");
-    console.log(thisColumn);
-    //console.log(columnsCopy);
-    //setLoading(true);
-    setColumns(columnsCopy);
-    setLoading(true);
-    console.log("ALL COLUMNS");
-    console.log(columns);
-    
-    // maybe instead issue a patch here and then refresh
-    //patchColumn(thisColumn, thisColumn.id);
-  }
+    setColumns(newColumns);
+  };
 
   const renderPage = () => {
     return (
       <DragDropContext onDragEnd={onDragEnd}>
         <Row gutter={16}>
           {columns.map((column) => (
-            <Col key={column.id} xs={{ span: 5, offset: 1 }} lg={{ span: 6, offset: 2 }}>
-              <KanbanColumnComponent columnID={column.id} /> 
-            </Col> 
+            <Col
+              key={column.id}
+              xs={{ span: 5, offset: 1 }}
+              lg={{ span: 6, offset: 2 }}
+            >
+              <KanbanColumnComponent column={column} />
+            </Col>
           ))}
         </Row>
       </DragDropContext>
     );
   };
 
-  return (
-    <div>
-      {renderPage()}
-    </div>
-  );
-
+  return <div>{renderPage()}</div>;
 }
 
 export default KanbanPageComponent;
