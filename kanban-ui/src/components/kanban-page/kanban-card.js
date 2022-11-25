b6c0c43cd92d6dce7524b90de46fda4f81db4fd4
William Gasper <wkg@williamgasper.com>
2022-11-25 07:29:58 -0600

    help from external stylesheet

--- kanban-ui/src/components/kanban-page/kanban-card.js
+++ kanban-ui/src/components/kanban-page/kanban-card.js
@@ -5,6 +5,7 @@ import { getCard } from "../../services/card";
 import { Draggable } from "react-beautiful-dnd";
 
 import "antd/dist/antd.css";
+import "./kanban-card.css";
 
 import { Card } from "antd";
 
@@ -14,9 +15,11 @@ function KanbanCardComponent({ card, index }) {
       <Draggable draggableId={card.id + ""} index={card.position}>
         {(provided) => (
           <Card
+            className="kanban-card"
+            hoverable
             size="small"
             title={card.name}
-            bordered={false}
+            bordered={true}
             {...provided.draggableProps}
             {...provided.dragHandleProps}
             ref={provided.innerRef}
