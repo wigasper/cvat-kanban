2634270869543b6df8f649c97e3e3b2cf0cd9803
William Gasper <wkg@williamgasper.com>
2022-12-02 09:45:17 -0600

    working on adding new data to cards

--- kanban-ui/src/components/kanban-page/kanban-card.js
+++ kanban-ui/src/components/kanban-page/kanban-card.js
@@ -5,9 +5,35 @@ import { Draggable } from "react-beautiful-dnd";
 import "antd/dist/antd.css";
 import "./kanban-card.css";
 
-import { Card } from "antd";
+import { Card, Col, Row } from "antd";
 
 function KanbanCardComponent({ card, index }) {
+  const getDifficulty = () => {
+    const colorMap = {0: "black",
+                      1: "green",
+                      2: "green",
+                      3: "yellow",
+                      4: "orange",
+                      5: "red"}
+
+    const bars = Array.from({length: card.difficulty}, () => "▋").join("");
+
+    return <span style={{color: colorMap[card.difficulty]}}>{bars}</span>;
+  }
+
+  const getThumbnail = () => {
+    if (card.thumbnail !== null) {
+      return <img 
+          width={64}
+          height={64}
+          alt={card.name}
+          src={card.thumbnail.image}
+        />;
+    }
+
+    return "";
+  }
+
   const renderCard = () => {
     return (
       <Draggable key={card.id} draggableId={card.id + ""} index={index}>
@@ -21,7 +47,17 @@ function KanbanCardComponent({ card, index }) {
             {...provided.draggableProps}
             {...provided.dragHandleProps}
             ref={provided.innerRef}
-          />
+          >
+            <Row>
+              <Col className="gutter-row">
+                {getThumbnail()}
+              </Col>
+              <Col className="gutter-row">
+                <p>Difficulty: {getDifficulty()}</p>
+                <p>Number structures: {card.num_structures}</p>
+              </Col>
+            </Row>
+          </Card>
         )}
       </Draggable>
     );
