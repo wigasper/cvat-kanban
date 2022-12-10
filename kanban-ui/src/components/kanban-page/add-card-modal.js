c5c1999639b34c34987591022236fd47d435d817
William Gasper <wkg@williamgasper.com>
2022-12-10 15:02:57 -0600

    add images with card

--- kanban-ui/src/components/kanban-page/add-card-modal.js
+++ kanban-ui/src/components/kanban-page/add-card-modal.js
@@ -2,23 +2,42 @@ import React, { useState } from "react";
 
 import "antd/dist/antd.css";
 
-import { Button, Modal, Form, Input } from "antd";
+import { Button, Modal, Form, Input, Upload } from "antd";
+import { UploadOutlined } from '@ant-design/icons';
 
-import { addCard } from "../../services/card";
+import { addCard, addCardMultipart } from "../../services/card";
+import { addThumbnail } from "../../services/thumbnail";
 
 function AddCardModalComponent({ columnID, onSubmit }) {
   const [modalOpen, setModalOpen] = useState(false);
   const [confirmModalLoading, setConfirmModalLoading] = useState(false);
   const [modalText, setModalText] = useState("tester");
+  const [thumbnail, setThumbnail] = useState(null);
+  const [thumbnailIm, setThumbnailIm] = useState(null);
 
   const [form] = Form.useForm();
 
   const onFinish = (values) => {
+    /*
     values["user"] = 1;
     values["column"] = columnID;
     values["board"] = 1;
+    values["thumbnail"] = thumbnailIm;
+    
+    debugger
 
     addCard(values);
+    */
+
+    let data = new FormData();
+    data.append("user", 1);
+    data.append("column", columnID);
+    data.append("board", 1);
+    data.append("thumbnail", thumbnailIm);
+    
+    data.append("name", values["name"]);
+
+    addCardMultipart(data);
     onSubmit(true);
   };
 
@@ -28,6 +47,41 @@ function AddCardModalComponent({ columnID, onSubmit }) {
 
   const showModal = () => {
     setModalOpen(true);
+  };
+  
+  const addImage = async options => {
+    const { onSuccess, onError, file, onProgress } = options;
+
+    let data = new FormData();
+
+    data.append("image", file);
+    
+    //try {addThumbnail(data).then((res) => {
+      //setThumbnail(res);
+    try {
+      setThumbnailIm(file);
+      debugger
+      onSuccess("Ok");
+    } catch (err) {
+      const error = new Error("Could not upload");
+      onError({ err });
+    }
+
+    /*
+    debugger
+
+    return fetch(`http://localhost:8000/kanban/thumbnails/`, {
+      method: "POST",
+      mode: "cors", 
+      body: data
+    })*/
+  };
+
+  const uploadProps = {
+    name: "file",
+    customRequest: addImage
+    //action: "http://localhost:8000/kanban/thumbnails/",
+    //crossOrigin: "anonymous"
   };
 
   const renderModal = () => {
@@ -51,6 +105,11 @@ function AddCardModalComponent({ columnID, onSubmit }) {
               ]}
             >
               <Input />
+            </Form.Item>
+            <Form.Item label="Upload">
+              <Upload {...uploadProps}>
+                <Button icon={<UploadOutlined />}>Click to upload</Button>
+              </Upload>
             </Form.Item>
           </Form>
           <p>{modalText}</p>
@@ -77,8 +136,12 @@ function AddCardModalComponent({ columnID, onSubmit }) {
 
   return (
     <div>
-      <Button type="dashed" block onClick={showModal} style={{ marginTop: 16 }}>
-        Add card
+      <Button type="dashed" 
+        block 
+        onClick={showModal} 
+        style={{ marginTop: 16 }}
+      >
+        <b>Add card</b>
       </Button>
       {renderModal()}
     </div>
